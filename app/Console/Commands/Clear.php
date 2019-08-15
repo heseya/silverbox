83df912c8ffee311df2bd7882fdde59a4ca5ca9b
Jędrzej Buliński <jedrzej@heseya.com>
2019-08-15 18:06:45 +0200

    :sparkles: Added command to removes files not associated with database

---
+++ app/Console/Commands/Clear.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\File;
+
+class Clear extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cdn:clear';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Removes files not associated with database';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $start = microtime(true);
+        $count = 0;
+
+        $path = storage_path(env('STORAGE_NAME', 'cdn'));
+        $files = scandir($path);
+
+        foreach($files as $file) {
+            
+            if(!preg_match('/\..*/', $file) && empty(File::find($file))) {
+                $count++;
+                unlink($path . '/' . $file);
+            }
+        }
+
+        echo 'Removed ' . $count . ' files in ' . round((microtime(true) - $start), 2) . " seconds\n";
+    }
+}
--- readme.md
+++ readme.md
@@ -1,3 +1,10 @@
 ## Documentation
 
 - [OpenAPI](https://bump.sh/doc/cdn)
+
+## Custom commands
+
+Removes files not associated with database
+```
+php artisan cdn:clear
+```
