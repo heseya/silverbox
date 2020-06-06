803277ad087f857c3d1b2949d47d3335d60f6ed5
Jędrzej Buliński <jedrzej@heseya.com>
2020-06-06 11:37:20 +0200

    Added byte unit liblary

--- app/Console/Commands/ClientsShow.php
+++ app/Console/Commands/ClientsShow.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use ByteUnits\Binary as ByteUnits;
 use Illuminate\Support\Facades\Storage;
 
 class ClientsShow extends Command
@@ -31,19 +32,19 @@ class ClientsShow extends Command
         $clients = [];
 
         foreach (Storage::directories() as $directory) {
-            $size = 0;
+            $size = ByteUnits::bytes(0);
 
             foreach (Storage::allFiles($directory) as $file) {
-                $size += Storage::size($file);
+                $size = $size->add(ByteUnits::bytes(Storage::size($file)));
             }
 
             $clients[] = [
                 $directory,
-                number_format($size / 1048576, 2) . ' MB',
+                $size->asMetric()->format(null, ' '),
                 Storage::exists($directory . DIRECTORY_SEPARATOR . '.key') ? 'yes' : 'no',
             ];
         }
 
-        $this->table(['client', 'size', 'key'], $clients);
+        $this->table(['client', 'usage', 'key'], $clients);
     }
 }
