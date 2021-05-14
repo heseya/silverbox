ca53d329d21376d89ff5a4d8b8e06fbff585b8e0
Bartosz Juraszewski <shadow38pl@gmail.com>
2021-05-14 02:11:29 +0200

    Disk cache, browser caching, docker

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -6,6 +6,7 @@ use App\Client;
 use App\File;
 use App\Http\Resources\FileResource;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Facades\Image;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
@@ -38,17 +39,33 @@ class ViewController extends BaseController
         }
 
         if ($file->mimeType() !== 'image/svg+xml' && ($request->has('w') || $request->has('h'))) {
-            $response = Image::cache(function ($image) use ($file, $request) {
-                return $image->make($file->binary())
+            $ext = pathinfo($fileName, PATHINFO_EXTENSION);
+            $fileName = pathinfo($fileName, PATHINFO_FILENAME);
+
+            $size = '_' . ($request->has('w') ? 'w_' . $request->input('w') : '') .
+                ($request->has('h') ? 'h_' . $request->input('h') : '');
+
+            $cacheName = 'cache' . DIRECTORY_SEPARATOR . $fileName . $size . '.' . $ext;
+            $cached = File::find($client, $cacheName);
+
+            if (!$cached) {
+                $image = Image::make($file->binary())
                 ->resize($request->input('w'), $request->input('h'), function ($constraint) {
                     $constraint->aspectRatio();
                     $constraint->upsize();
-                });
-            });
+                })->encode();
+                
+                Storage::put($client . DIRECTORY_SEPARATOR . $cacheName, $image);
+
+                $cached = File::find($client, $cacheName);
+            }
+
+            $file = $cached;
         }
 
-        return response($response ?? $file->binary())
-            ->header('Content-Type', $file->mimeType());
+        return response($file->binary())
+            ->header('Content-Type', $file->mimeType())
+            ->header('Cache-Control', 'public, max-age=' . env('CACHE_TIME', 15552000)); // default 6 months
     }
 
     /**
